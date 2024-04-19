1cc7d936b95a38cdafdf1b962f5e1bdca64334db
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-04-19 14:52:33 +0200

    Added social media links

--- src/SocialMedia.js
+++ src/SocialMedia.js
@@ -6,10 +6,18 @@ export default function () {
     <div className="SocialMedia">
       <h3>matea.zerjav@gmail.com</h3>
 
-      <a href="https://www.linkedin.com/in/matea-zerjav-482b93272/">
+      <a
+        href="https://www.linkedin.com/in/matea-zerjav-482b93272/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fa-brands fa-linkedin"></i>
       </a>
-      <a href="https://github.com/MateaZerjav">
+      <a
+        href="https://github.com/MateaZerjav"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fa-brands fa-square-github"></i>
       </a>
     </div>
