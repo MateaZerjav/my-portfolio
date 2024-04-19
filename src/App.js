08f55fd85b0a3a449c14b11b513ba88451970ad0
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-04-19 11:39:33 +0200

    Make Hero responsiv

--- src/App.js
+++ src/App.js
@@ -9,12 +9,23 @@ function App() {
       <header className="App-header">
         <div className="container text-center mt-5 ">
           <div className="row">
-            <div className="col">
+            <div className="col-12">
+              <img
+                className="img-fluid d-block d-sm-none  MateasPhoto"
+                src={photo}
+                alt="Matea"
+              />
+            </div>
+            <div className="col-sm col-12 ">
               {" "}
               <Hero />
             </div>
             <div className="col">
-              <img className="img-fluid  MateasPhoto" src={photo} alt="Matea" />
+              <img
+                className="img-fluid d-none d-sm-block MateasPhoto"
+                src={photo}
+                alt="Matea"
+              />
             </div>
           </div>
         </div>
