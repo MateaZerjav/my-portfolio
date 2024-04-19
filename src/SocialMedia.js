03df920eed2e56480e64fd1827e95452ac8d9ae9
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-04-19 18:17:11 +0200

    Deletetd social links

--- src/SocialMedia.js
+++ src/SocialMedia.js
@@ -1,27 +1,10 @@
 import React from "react";
 import "./SocialMedia.css";
 
-export default function () {
+export default function SocialMedia() {
   return (
     <div className="SocialMedia">
       <h3>matea.zerjav@gmail.com</h3>
-
-      <a
-        className="icon"
-        href="https://www.linkedin.com/in/matea-zerjav-482b93272/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fa-brands fa-linkedin"></i>
-      </a>
-      <a
-        className="icon"
-        href="https://github.com/MateaZerjav"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fa-brands fa-square-github"></i>
-      </a>
       <p className="codedBy">
         This website was created using React.js and developed by Matea Zerjav,
         and is
