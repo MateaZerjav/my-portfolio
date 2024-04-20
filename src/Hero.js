1b514b5ac07d9c1e91f0cc0b36a6d1480f663200
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-04-20 09:55:00 +0200

    Updated Hero.js

--- src/Hero.js
+++ src/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className="Hero">
       <h1>Hi, I am Matea Žerjav</h1>
-      <h2>Frontend Developer, based in Stuttgart, Germany.</h2>
+      <h2>Junior Frontend Developer, based in Stuttgart, Germany.</h2>
       <p>Let’s work together and i’ll help you by all my best</p>
       <a href="/#contact-matea">Get Started</a>
     </div>
