147587f843b12d2e85f31264ff443fb6cb7c9e88
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-05-16 15:30:37 +0200

    Added page on german and croatian

--- src/App.js
+++ src/App.js
@@ -1,91 +1,18 @@
 import "./App.css";
-import Hero from "./components/Hero";
-import "bootstrap/dist/css/bootstrap.min.css";
-import photo from "./photos/photo.jpg";
-import ContactMe from "./components/ContactMe";
-import SocialMedia from "./components/SocialMedia";
-import Projects from "./components/Projects";
-import photo_poetry from "./photos/AI Croatian poetry generator.png";
-import photo_dictionary from "./photos/Dictionary App.png";
-import photo_react_weather from "./photos/React-Weather-app.png";
-import photo_weather from "./photos/Weather app.png";
-import Tehnologies from "./components/Tehnologies";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Englisch from "./pages/englisch";
+import German from "./pages/german";
+import Croatian from "./pages/croatian";
 
 function App() {
   return (
-    <div className="App">
-      <style>{"body{background-color: rgba(7, 67, 10, 0.196);}"}</style>
-      <header className="App-header">
-        <div className="container text-center mt-5 ">
-          <div className="row">
-            <div className="col-12">
-              <img
-                className="img-fluid d-block d-sm-none  MateasPhoto"
-                src={photo}
-                alt="Matea"
-              />
-            </div>
-            <div className="col-sm col-12 ">
-              {" "}
-              <Hero />
-            </div>
-            <div className="col">
-              <img
-                className="img-fluid d-none d-sm-block MateasPhoto"
-                src={photo}
-                alt="Matea"
-              />
-            </div>
-          </div>
-        </div>
-      </header>
-      <main>
-        <Tehnologies />
-        <h4>Discover My Work: Browse Through My Projects</h4>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <Projects
-                Header="Dictionary App"
-                Title="Created using React.js, an active API, responsive development, and CSS"
-                photo={photo_dictionary}
-                link="https://dictionary-final-project.netlify.app/"
-              />
-            </div>
-            <div className="col-sm col-12">
-              <Projects
-                Header="Weather App"
-                Title="Created using React.js, responsive development, and an active API"
-                photo={photo_react_weather}
-                link="https://master--stupendous-alpaca-4c667b.netlify.app/"
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <Projects
-                Header="Poetry generator"
-                Title="Built using HTML, an active API, responsive development, and CSS and JavaScript"
-                photo={photo_poetry}
-                link="https://croatianversecraft.netlify.app/"
-              />
-            </div>
-            <div className="col-sm col-12">
-              <Projects
-                Header="Weather App"
-                Title="Built using HTML, an active API, and advanced CSS and JavaScript"
-                photo={photo_weather}
-                link="https://main--phenomenal-dolphin-9ca8ea.netlify.app/"
-              />
-            </div>
-          </div>
-        </div>
-      </main>
-      <footer id="contact-matea">
-        <ContactMe />
-        <SocialMedia />
-      </footer>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Englisch />} />
+        <Route path="/german" element={<German />} />
+        <Route path="/croatian" element={<Croatian />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/german.jsx
@@ -0,0 +1,94 @@
+import "./portfolio.css";
+import Hero from "../components/Hero";
+import "bootstrap/dist/css/bootstrap.min.css";
+import photo from "../photos/photo.jpg";
+import ContactMe from "../components/ContactMe";
+import SocialMedia from "../components/SocialMedia";
+import Projects from "../components/Projects";
+import photo_poetry from "../photos/AI Croatian poetry generator.png";
+import photo_dictionary from "../photos/Dictionary App.png";
+import photo_react_weather from "../photos/React-Weather-app.png";
+import photo_weather from "../photos/Weather app.png";
+import Tehnologies from "../components/Tehnologies";
+import { Link } from "react-router-dom";
+
+export default function German() {
+  return (
+    <div className="App">
+      <style>{"body{background-color: rgba(7, 67, 10, 0.196);}"}</style>
+      <header className="App-header">
+        <div className="container text-center mt-5 ">
+          <div className="row">
+            <div className="col-12">
+              <img
+                className="img-fluid d-block d-sm-none  MateasPhoto"
+                src={photo}
+                alt="Matea"
+              />
+            </div>
+            <div className="col-sm col-12 ">
+              {" "}
+              <Hero />
+            </div>
+            <div className="col">
+              <img
+                className="img-fluid d-none d-sm-block MateasPhoto"
+                src={photo}
+                alt="Matea"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <Tehnologies />
+        <h4>njemacki: Browse Through My Projects</h4>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Projects
+                Header="Dictionary App"
+                Title="Created using React.js, an active API, responsive development, and CSS"
+                photo={photo_dictionary}
+                link="https://dictionary-final-project.netlify.app/"
+              />
+            </div>
+            <div className="col-sm col-12">
+              <Projects
+                Header="Weather App"
+                Title="Created using React.js, responsive development, and an active API"
+                photo={photo_react_weather}
+                link="https://master--stupendous-alpaca-4c667b.netlify.app/"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <Projects
+                Header="Poetry generator"
+                Title="Built using HTML, an active API, responsive development, and CSS and JavaScript"
+                photo={photo_poetry}
+                link="https://croatianversecraft.netlify.app/"
+              />
+            </div>
+            <div className="col-sm col-12">
+              <Projects
+                Header="Weather App"
+                Title="Built using HTML, an active API, and advanced CSS and JavaScript"
+                photo={photo_weather}
+                link="https://main--phenomenal-dolphin-9ca8ea.netlify.app/"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer id="contact-matea">
+        <ContactMe />
+        <SocialMedia />
+        <Link to="/">Englisch</Link>
+        <Link to="/german">German</Link>
+        <Link to="/croatian">Croatian</Link>
+      </footer>
+    </div>
+  );
+}
