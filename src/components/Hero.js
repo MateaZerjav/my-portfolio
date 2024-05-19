01ef7e33b651d085f2b0ad566d48c2b16c4cd28d
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-05-19 14:55:05 +0200

    Deleted button from Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Hero.css";
-
+import { Link } from "react-router-dom";
 export default function Hero(props) {
   return (
     <div className="Hero">
       <h1>{props.header1}</h1>
       <h2>{props.header2}</h2>
       <p>{props.paragraph}</p>
-      <button className="button">{props.getstarted}</button>
     </div>
   );
 }
