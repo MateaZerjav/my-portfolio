c45c7ef8d5c51d870d391f3251af5135fb74761f
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-06-02 17:53:26 +0200

    Made Work Inquiry form responsiv

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,17 +8,16 @@ export default function Hero(props) {
       <h1>{props.header1}</h1>
       <h2>{props.header2}</h2>
       <p>{props.paragraph}</p>
-      <button>
-        <Link
-          activeClass="active"
-          to={props.scroll}
-          smooth={true}
-          offset={50}
-          duration={500}
-        >
-          {props.button}
-        </Link>
-      </button>
+
+      <Link
+        activeClass="active"
+        to={props.scroll}
+        smooth={true}
+        offset={50}
+        duration={500}
+      >
+        <button>{props.button}</button>
+      </Link>
     </div>
   );
 }
--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -6,8 +6,8 @@ export default function MyForm(props) {
     <div className="MyForm">
       <style>{"body{background-color: rgba(7, 67, 10, 0.196);}"}</style>
       <div className="container form">
-        <div className="form_container">
-          <h1 className="">{props.header}</h1>
+        <div className="form_container_lg  d-none d-lg-block ">
+          <h1>{props.header}</h1>
           <form method="POST" action="https://getform.io/f/lbkmzlyb">
             <div className="row">
               <div class="col ">
@@ -34,6 +34,48 @@ export default function MyForm(props) {
               </div>
               <div class="col">
                 <button type="submit">{props.submit}</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className=" form_container_sm 	d-block d-lg-none ">
+          <h1 className="">{props.header}</h1>
+          <form method="POST" action="https://getform.io/f/lbkmzlyb">
+            <div className="container">
+              <div className="row">
+                <div class="col ">
+                  <input
+                    name="Name"
+                    type="text"
+                    placeholder={props.enter_name}
+                    className="border border-0 "
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <input
+                    name="Email"
+                    type="email"
+                    placeholder={props.enter_email}
+                    className="border border-0 e"
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div class="col">
+                  <input
+                    name="message"
+                    type="text"
+                    placeholder={props.enter_message}
+                    className="border border-0  messege-sm "
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div class="col">
+                  <button type="submit">{props.submit}</button>
+                </div>
               </div>
             </div>
           </form>
